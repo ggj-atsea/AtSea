8dcc88e3858ec24f1121b0e69673599fb19a7039
Jeremy Nikolai <jeremy@controlledpanic.com>
2017-01-21 16:28:00 -0800

    Player States

--- Assets/Code/Controllers/PlayerController.cs
+++ Assets/Code/Controllers/PlayerController.cs
@@ -1,7 +1,12 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿// 'variable never assigned' for serialized fields
+#pragma warning disable 0649
 
-public class PlayerController : MonoBehaviour
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : Singleton<PlayerController>
 {
     public float Hunger { get; private set; }
     public float Thirst { get; private set; }
@@ -22,14 +27,21 @@ public class PlayerController : MonoBehaviour
         Hunger = 0.0f;
         Thirst = 0.0f;
 
+        DayNightController.Instance.OnDusk += OnDusk;
         DayNightController.Instance.OnMidnight += OnMidnight;
         DayNightController.Instance.OnDawn += OnDawn;
 	}
 
     void OnDestroy() {
-        if (DayNightController.HasInstance)
+        if (DayNightController.HasInstance) {
+            DayNightController.Instance.OnDusk -= OnDusk;
             DayNightController.Instance.OnMidnight -= OnMidnight;
             DayNightController.Instance.OnDawn -= OnDawn;
+        }
+    }
+
+    void OnDusk(int day) {
+        In(3.0f, () => SetState("asleep"));
     }
 
     void OnMidnight(int day) {
@@ -53,6 +65,8 @@ public class PlayerController : MonoBehaviour
             UI.Instance.SetDay(day);
 
             HUD.Instance.UpdateStats(this);
+
+            In(1.0f, () => SetState("standing"));
         }
     }
 
@@ -61,6 +75,8 @@ public class PlayerController : MonoBehaviour
         Hunger -= 5.0f;
         if(Hunger <= 0)
             Hunger = 0;
+ 
+        HUD.Instance.UpdateStats(this);
     }
 
     public void DrinkWater()
@@ -68,5 +84,34 @@ public class PlayerController : MonoBehaviour
         Thirst -= 5.0f;
         if(Thirst <= 0)
             Thirst = 0;
+ 
+        HUD.Instance.UpdateStats(this);
     }
+
+    private void In(float seconds, Action action) {
+        StartCoroutine(InCo(seconds, action));
+    }
+    private IEnumerator InCo(float seconds, Action action) {
+        yield return new WaitForSeconds(seconds);
+        action();
+    }
+
+    #region State Management
+
+    [System.Serializable]
+    private class State {
+        public string _name;
+        public GameObject _state;
+    }
+
+    [SerializeField] private State[] _playerStates;
+
+    public void SetState(string name) {
+        foreach (var state in _playerStates) {
+            if (state._state != null)
+                state._state.SetActive(state._name == name);
+        }
+    }
+
+    #endregion
 }
