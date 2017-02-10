11f3568c40df6372f8de65d1e08bc1de6e23556f
Jason <jeala510@gmail.com>
2017-02-09 21:11:20 -0800

    Add buoyancy controller

---
+++ Assets/Code/Controllers/BuoyancyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BuoyancyController : MonoBehaviour {
+
+	// The higher the force, the faster it moves
+	[SerializeField] private float _wobbleForce = 40;
+
+	void OnEnable()
+	{
+		StartCoroutine(LoopRotation());
+	}
+
+	void OnDisable()
+	{
+		StopCoroutine(LoopRotation());
+	}
+
+	IEnumerator LoopRotation()
+	{
+		float rot = 0f;
+		float dir = 1f;
+		while(true)
+		{
+			while(rot < 75)
+			{
+				float step = Time.deltaTime * _wobbleForce;
+				transform.Rotate(new Vector3(0,0,1) * step * dir);
+				rot += 1;
+				yield return null;
+			}
+			rot= 0f;
+			dir *= -1f;
+		}
+	}
+
+}
